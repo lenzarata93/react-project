9e4fb6df9df515e7961590a22da2d73154123d70
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-15 10:57:04 +0200

    Add Places-List component with css(not working corectly)

--- client/src/components/Places/Places-List.jsx
+++ client/src/components/Places/Places-List.jsx
@@ -1,10 +1,22 @@
+import styles from '../../components/Places/Places-List.module.css'
+
 export default function PlacesList(){
 return(
-
-    <article>
+<div className='places-container'>
+<article className='places'>
         <h2>Име</h2>
-        <img href="https://www.btsbg.org/sites/default/files/styles/768x576/public/obekti/vruh-vihren.jpg?itok=QiMyjWfE"></img>
+        <img href="" src="https://www.btsbg.org/sites/default/files/styles/768x576/public/obekti/peshtera-magurata.jpg?itok=Oo6Htpsb"></img>
+        <h2>Местоположение</h2>
         <p>Описание</p>
     </article>
+
+<article className="places">
+<h2>Име</h2>
+<img href="" src="https://blog.luxury-discounts.com/wp-content/uploads/%D0%A8%D0%98%D0%9F%D0%9A%D0%90-1-1536x1106.jpg"></img>
+<h2>Местоположение</h2>
+<p>Описание</p>
+</article>
+</div>
+
 )
 };
