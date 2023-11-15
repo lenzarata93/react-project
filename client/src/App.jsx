eb9fe9676f39259c9af26f5067558b8890b3ffe0
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-15 10:17:29 +0200

    Add Place-List Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./components/Navigation"
 import Footer from "./components/Footer"
+import PlacesList from "./components/Places/Places-List"
 
 function App() {
  
   return (
     <div id="page">
      <Navigation />
+     <PlacesList />
      <Footer />
     </div>
   )
---
+++ client/src/components/Places/Places-List.jsx
@@ -0,0 +1,10 @@
+export default function PlacesList(){
+return(
+
+    <article>
+        <h2>Име</h2>
+        <img href="https://www.btsbg.org/sites/default/files/styles/768x576/public/obekti/vruh-vihren.jpg?itok=QiMyjWfE"></img>
+        <p>Описание</p>
+    </article>
+)
+};
