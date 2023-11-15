4d3c34f6b4b0564589fb124abc228c2a2a16b31e
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-15 10:11:07 +0200

    update nav

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -5,8 +5,8 @@ export default function Navigation(){
 <nav>
   <ul>
     <li><a href="">Начало</a></li>
-    <li><a href="">Новини</a></li>
-    <li><a href="">Контакт</a></li>
+    <li><a href="">Обекти</a></li>
+    <li><a href="">Добави обект</a></li>
     <li><a href="">За нас</a></li>
     <li><a href="">Регистрация</a></li>
     <li><a href="">Вход</a></li>
