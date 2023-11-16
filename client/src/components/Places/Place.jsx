41a1a4cdc93486b1b1fa277db81d28972f598e2b
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-16 17:11:57 +0200

    Add Place component

---
+++ client/src/components/Places/Place.jsx
@@ -0,0 +1,11 @@
+export default function Place(){
+return(
+    <article className="article">
+        <img href="" src="https://www.btsbg.org/sites/default/files/styles/768x576/public/obekti/peshtera-magurata.jpg?itok=Oo6Htpsb"></img>
+        <h2>Име</h2>
+        <p>Описание</p>
+        <h2>Местоположение</h2>
+    </article>
+)
+
+};
--- client/src/components/Places/Places-List.jsx
+++ client/src/components/Places/Places-List.jsx
@@ -3,12 +3,7 @@ import styles from '../../components/Places/Places-List.module.css'
 export default function PlacesList(){
 return(
 <div className='places-container'>
-<article className="article">
-        <img href="" src="https://www.btsbg.org/sites/default/files/styles/768x576/public/obekti/peshtera-magurata.jpg?itok=Oo6Htpsb"></img>
-        <h2>Име</h2>
-        <p>Описание</p>
-        <h2>Местоположение</h2>
-    </article>
+
 
 <article className="article">
 <img href="" src="https://blog.luxury-discounts.com/wp-content/uploads/%D0%A8%D0%98%D0%9F%D0%9A%D0%90-1-1536x1106.jpg"></img>
